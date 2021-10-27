c072019fecf6ed360f6117e1081387509d9421c8
olaniyi <olaniyikolawole744@gmail.com>
2021-10-27 10:21:45 -0400

    push to repo

--- scripts/playbook.sh
+++ scripts/playbook.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd ansible-jobs
 ls
-ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook ansible-jobs/playbook/playprod.yml -i ansible-jobs/inventory/hosts/ec2.py -vvvvv
+ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook playbook/playprod.yml -i inventory/hosts/ec2.py -vvvvv
