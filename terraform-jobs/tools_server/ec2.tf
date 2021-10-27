c38d65dc564252c95e9746d931ba4bc84bed6b16
olaniyi <olaniyikolawole744@gmail.com>
2021-10-27 08:24:43 -0400

    push to repo

---
+++ terraform-jobs/tools_server/ec2.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "us-east-1"
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.server_ami.id
+  instance_type = "t2.micro"
+  key_name = "new-devops-key"
+  tags = {
+    Name = "tools_server"
+  }
+}
