279e7faba8b232359f88da07665eb931fe3f67df
olaniyi <olaniyikolawole744@gmail.com>
2021-10-28 01:35:34 -0400

    push to repo

--- scripts/playbook.sh
+++ scripts/playbook.sh
@@ -4,4 +4,4 @@ ls
 chmod 755 inventory/hosts/ec2.py
 chmod 755 inventory/hosts/ec2.ini
 echo "'${ANSIBLE_VAULT_PASSWORD_FILE}' > secret.txt"
-ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook playbook/playprod.yml -i inventory/hosts/ec2.py --vault-password-file secret.txt -vvvvv
+ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook playbook/playprod.yml -i inventory/hosts/ec2.py --vault-password-file $ANSIBLE_VAULT_PASSWORD_FILE -vvvvv
