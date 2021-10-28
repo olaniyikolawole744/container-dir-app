344d750bf506984d803156cbc7ae3f75d042942a
olaniyi <olaniyikolawole744@gmail.com>
2021-10-28 05:27:21 -0400

    push to repo

--- scripts/playbook.sh
+++ scripts/playbook.sh
@@ -3,4 +3,4 @@ cd ansible-jobs
 ls
 chmod 755 inventory/hosts/ec2.py
 chmod 755 inventory/hosts/ec2.ini
-ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook playbook/playprod.yml -i inventory/hosts/ec2.py --vault-password-file $ANSIBLE_VAULT_PASSWORD_FILE 
+ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook playbook/playdev.yml -i inventory/hosts/ec2.py --vault-password-file $ANSIBLE_VAULT_PASSWORD_FILE 
