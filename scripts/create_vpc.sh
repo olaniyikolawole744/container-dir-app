914f0e3cc25fd650eb53e959e920b7fba05581dd
olaniyi <olaniyikolawole744@gmail.com>
2021-11-27 07:55:09 -0500

    push

--- scripts/create_vpc.sh
+++ scripts/create_vpc.sh
@@ -5,4 +5,4 @@ cd terraform-jobs
 cd infrastructure_vpc
 ls
 docker-compose -f docker-compose.yml run --rm terraform init
-docker-compose -f docker-compose.yml run --rm terraform apply "-var-file=variable_values.tfvars" --auto-approve
+docker-compose -f docker-compose.yml run --rm terraform apply --auto-approve
