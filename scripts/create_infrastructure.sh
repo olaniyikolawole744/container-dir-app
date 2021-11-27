c27faaa4a10246182fb54128b08abee670738b97
olaniyi <olaniyikolawole744@gmail.com>
2021-11-27 07:41:27 -0500

    push to repo

--- scripts/create_infrastructure.sh
+++ scripts/create_infrastructure.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-ls
 cd terraform-jobs
 cd remote_servers
+ls
 docker-compose -f docker-compose.yml run --rm terraform init
 docker-compose -f docker-compose.yml run --rm terraform apply "-var-file=variable_values.tfvars" --auto-approve
 
 cd ..
 cd subnet_rt_assoc
+ls
 docker-compose -f docker-compose.yml run --rm terraform init
 docker-compose -f docker-compose.yml run --rm terraform apply "-var-file=variable_values.tfvars" --auto-approve
--- scripts/create_vpc.sh
+++ scripts/create_vpc.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # CREATE VPC
-ls
 cd terraform-jobs
 cd infrastructure_vpc
+ls
 docker-compose -f docker-compose.yml run --rm terraform init
 docker-compose -f docker-compose.yml run --rm terraform apply "-var-file=variable_values.tfvars" --auto-approve
