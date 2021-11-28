babde767a6648730732cb3503bfb0b7d93db96c5
olaniyi <olaniyikolawole744@gmail.com>
2021-11-28 14:50:22 -0500

    push

--- scripts/create_vpc.sh
+++ scripts/create_vpc.sh
@@ -4,5 +4,5 @@
 cd terraform-jobs
 cd infrastructure_vpc
 ls
-docker compose -f docker-compose.yml run --rm terraform init
-docker compose -f docker-compose.yml run --rm terraform apply --auto-approve
+docker-compose -f docker-compose.yml run --rm terraform init
+docker-compose -f docker-compose.yml run --rm terraform apply --auto-approve
