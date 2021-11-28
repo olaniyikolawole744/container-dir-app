6494de5c803ffbf059b7a4b073ba1b0a078423a1
olaniyi <olaniyikolawole744@gmail.com>
2021-11-28 14:56:24 -0500

    push

--- scripts/create_infrastructure.sh
+++ scripts/create_infrastructure.sh
@@ -2,11 +2,11 @@
 cd terraform-jobs
 cd remote_servers
 ls
-docker-compose -f docker-compose.yml run --rm terraform init
-docker-compose -f docker-compose.yml run --rm terraform apply "-var-file=variable_values.tfvars" --auto-approve
+terraform init
+terraform apply "-var-file=variable_values.tfvars" --auto-approve
 
 cd ..
 cd subnet_rt_assoc
 ls
-docker-compose -f docker-compose.yml run --rm terraform init
-docker-compose -f docker-compose.yml run --rm terraform apply "-var-file=variable_values.tfvars" --auto-approve
+terraform init
+terraform apply "-var-file=variable_values.tfvars" --auto-approve
