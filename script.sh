c689f5b8c05a6db8d676191562031070805a4849
Olaniyi Kolawole <olaniyikolawole@Olaniyis-MacBook-Pro.local>
2021-12-31 01:41:19 -0500

    push

--- script.sh
+++ script.sh
@@ -34,6 +34,6 @@ echo "run ansible playbook......................................................
 chmod 755 ansible-jobs/inventory/hosts/ec2.py
 ANSIBLE_HOSTS=ansible-jobs/inventory/ec2.py
 EC2_INI_PATH=ansible-jobs/inventory/ec2.ini
-ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook --vault-password-file ansible-jobs/dockerhub_credentials ansible-jobs/playbook/playdev.yml -i ansible-jobs/inventory/hosts/ec2.py
+ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook --vault-password-file ansible-jobs/playbook/dockerhub_credentials ansible-jobs/playbook/playdev.yml -i ansible-jobs/inventory/hosts/ec2.py
 
 
